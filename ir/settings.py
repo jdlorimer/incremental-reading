96fd67b3a187b1e9c3f0a0149ac7559bc8ba603c
luoliyan <joseph.lorimer@posteo.net>
2017-01-10 13:46:02 +0930

    Store settings internally & merge IRE & VSA settings file

--- ir/settings.py
+++ ir/settings.py
@@ -8,28 +8,34 @@ import ir.util
 
 
 class SettingsManager():
-    def getSettings(self):
+    def __init__(self):
+        self.loadSettings()
+
+    def loadSettings(self):
         self.mediaDir = os.path.join(aqt.mw.pm.profileFolder(),
                                      'collection.media')
         self.jsonPath = os.path.join(self.mediaDir, '_ir.json')
 
         if os.path.isfile(self.jsonPath):
             with codecs.open(self.jsonPath, encoding='utf-8') as jsonFile:
-                return json.load(jsonFile)
+                self.settings = json.load(jsonFile)
         else:
-            return {'doHighlightFont': 'false',
-                    'highlightColor': 'yellow',
-                    'schedLaterInt': 50,
-                    'schedLaterRandom': True,
-                    'schedLaterType': 'pct',
-                    'schedSoonInt': 10,
-                    'schedSoonRandom': True,
-                    'schedSoonType': 'pct',
-                    'zoomAndScroll': {}}
+            self.settings = {'doHighlightFont': 'false',
+                             'highlightColor': 'yellow',
+                             'lastDialogQuickKey': {},
+                             'quickKeys': {},
+                             'schedLaterInt': 50,
+                             'schedLaterRandom': True,
+                             'schedLaterType': 'pct',
+                             'schedSoonInt': 10,
+                             'schedSoonRandom': True,
+                             'schedSoonType': 'pct',
+                             'textSizeMultiplier': 1,
+                             'zoomAndScroll': {}}
 
-    def saveSettings(self, settings):
+    def saveSettings(self):
         with codecs.open(self.jsonPath, 'w', encoding='utf-8') as jsonFile:
-            json.dump(settings, jsonFile)
+            json.dump(self.settings, jsonFile)
 
         # Touch the media folder to force sync
         ir.util.updateModificationTime(self.mediaDir)
