c0d00aa51e7334145239199d1b417544ef11c4bb
luoliyan <joseph.lorimer13@gmail.com>
2017-01-12 10:04:28 +0930

    Add helper functions for adding menu items & shortcuts

--- ir/util.py
+++ ir/util.py
@@ -2,6 +2,47 @@ import os
 import stat
 import time
 
+from PyQt4.QtCore import SIGNAL
+from PyQt4.QtGui import QAction, QKeySequence, QMenu, QShortcut
+from aqt import mw
+
+
+def addMenu(name):
+    if not hasattr(mw, 'customMenus'):
+        mw.customMenus = {}
+
+    if name not in mw.customMenus:
+        menu = QMenu('&' + name, mw)
+        mw.customMenus[name] = menu
+        mw.form.menubar.insertMenu(mw.form.menuTools.menuAction(),
+                                   mw.customMenus[name])
+
+
+def addMenuItem(menuName, text, function, keys=None):
+    action = QAction(text, mw)
+
+    if keys:
+        action.setShortcut(QKeySequence(keys))
+
+    mw.connect(action, SIGNAL('triggered()'), function)
+
+    if menuName == 'File':
+        mw.form.menuCol.addAction(action)
+    elif menuName == 'Edit':
+        mw.form.menuEdit.addAction(action)
+    elif menuName == 'Tools':
+        mw.form.menuTools.addAction(action)
+    elif menuName == 'Help':
+        mw.form.menuHelp.addAction(action)
+    else:
+        addMenu(menuName)
+        mw.customMenus[menuName].addAction(action)
+
+
+def addShortcut(function, keys):
+    shortcut = QShortcut(QKeySequence(keys), mw)
+    mw.connect(shortcut, SIGNAL('activated()'), function)
+
 
 def updateModificationTime(path):
     accessTime = os.stat(path)[stat.ST_ATIME]
