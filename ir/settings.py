8e93376e5847d8f586b16cf2f2bbad2a5a43bdef
luoliyan <joseph.lorimer13@gmail.com>
2017-02-28 17:54:52 +0930

    Allow non-ASCII in path under Python 2

--- ir/settings.py
+++ ir/settings.py
@@ -7,12 +7,14 @@ import json
 import os
 
 try:
+    ANKI_21 = False
     from PyQt4.QtCore import Qt
     from PyQt4.QtGui import (QButtonGroup, QCheckBox, QComboBox, QDialog,
                              QDialogButtonBox, QGroupBox, QHBoxLayout, QLabel,
                              QLineEdit, QPushButton, QRadioButton, QSpinBox,
                              QTabWidget, QVBoxLayout, QWidget)
 except ImportError:
+    ANKI_21 = True
     from PyQt5.QtCore import Qt
     from PyQt5.QtWidgets import (QButtonGroup, QCheckBox, QComboBox, QDialog,
                                  QDialogButtonBox, QGroupBox, QHBoxLayout,
@@ -419,6 +421,8 @@ class SettingsManager():
 
     def getColorList(self):
         moduleDir, _ = os.path.split(__file__)
+        if not ANKI_21:
+            moduleDir = moduleDir.decode('utf-8')
         colorsFilePath = os.path.join(moduleDir, 'data', 'colors.u8')
         with codecs.open(colorsFilePath, encoding='utf-8') as colorsFile:
             return [line.strip() for line in colorsFile]
