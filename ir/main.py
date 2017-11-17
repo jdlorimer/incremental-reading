fefe0c4247e2def3a54c127ce7476629daa63892
luoliyan <joseph.lorimer13@gmail.com>
2017-11-17 11:28:31 +0930

    Overhaul quick keys; allow custom tags
    * Merge quick keys and extraction logic to eliminate duplicate code
    * Allow custom tags for quick key extraction; tags box added

--- ir/main.py
+++ ir/main.py
@@ -1,11 +1,6 @@
-from anki import notes
 from anki.hooks import addHook, wrap
-from anki.sound import clearAudioQueue
 from aqt import mw
-from aqt.addcards import AddCards
-from aqt.editcurrent import EditCurrent
 from aqt.reviewer import Reviewer
-from aqt.utils import showWarning, tooltip
 
 import sip
 
@@ -14,11 +9,7 @@ from .importer import Importer
 from .schedule import Scheduler
 from .settings import SettingsManager
 from .text import TextManager
-from .util import (addMenuItem,
-                   getField,
-                   isIrCard,
-                   setField,
-                   viewingIrText)
+from .util import addMenuItem, isIrCard
 from .view import ViewManager
 
 
@@ -46,16 +37,16 @@ class ReadingManager:
         self.viewManager.resetZoom('deckBrowser')
         self.addModel()
         self.loadMenuItems()
-        self.shortcuts = [(self.settings['extractKey'],
-                           self.textManager.extract),
-                          (self.settings['highlightKey'],
-                          self.textManager.highlight),
-                          (self.settings['removeKey'], self.textManager.remove),
-                          (self.settings['undoKey'], self.textManager.undo),
-                          ('Up', self.viewManager.lineUp),
-                          ('Down', self.viewManager.lineDown),
-                          ('PgUp', self.viewManager.pageUp),
-                          ('PgDown', self.viewManager.pageDown)]
+        self.shortcuts = [
+            ('Down', self.viewManager.lineDown),
+            ('PgDown', self.viewManager.pageDown),
+            ('PgUp', self.viewManager.pageUp),
+            ('Up', self.viewManager.lineUp),
+            (self.settings['extractKey'], self.textManager.extract),
+            (self.settings['highlightKey'], self.textManager.highlight),
+            (self.settings['removeKey'], self.textManager.remove),
+            (self.settings['undoKey'], self.textManager.undo),
+        ]
 
     def loadMenuItems(self):
         if hasattr(mw, 'customMenus') and 'Read' in mw.customMenus:
@@ -131,70 +122,6 @@ class ReadingManager:
 
         mw.col.models.addTemplate(model, template)
         mw.col.models.add(model)
-
-    def quickAdd(self, quickKey):
-        if not viewingIrText():
-            return
-
-        self.currentQuickKey = quickKey
-
-        if quickKey['plainText']:
-            mw.web.evalWithCallback('getPlainText()', self.createNote)
-        else:
-            mw.web.evalWithCallback('getHtmlText()', self.createNote)
-
-    def createNote(self, selectedText):
-        self.textManager.highlight(self.currentQuickKey['bgColor'],
-                                   self.currentQuickKey['textColor'])
-
-        newModel = mw.col.models.byName(self.currentQuickKey['modelName'])
-        newNote = notes.Note(mw.col, newModel)
-        setField(newNote, self.currentQuickKey['fieldName'], selectedText)
-
-        card = mw.reviewer.card
-        currentNote = card.note()
-        tags = currentNote.stringTags()
-        # Sets tags for the note, but still have to set them in the editor
-        #   if show dialog (see below)
-        newNote.setTagsFromStr(tags)
-
-        for f in newModel['flds']:
-            if self.settings['sourceField'] == f['name']:
-                setField(newNote,
-                         self.settings['sourceField'],
-                         getField(currentNote, self.settings['sourceField']))
-
-        if self.currentQuickKey['editExtract']:
-            addCards = AddCards(mw)
-            addCards.editor.setNote(newNote)
-            if newNote.stringTags():
-                addCards.editor.tags.setText(newNote.stringTags().strip())
-            addCards.modelChooser.models.setText(
-                self.currentQuickKey['modelName'])
-            addCards.deckChooser.deck.setText(
-                self.currentQuickKey['deckName'])
-        else:
-            deckId = mw.col.decks.byName(self.currentQuickKey['deckName'])['id']
-            newNote.model()['did'] = deckId
-            ret = newNote.dupeOrEmpty()
-            if ret == 1:
-                showWarning(_(
-                    'The first field is empty.'),
-                    help='AddItems#AddError')
-                return
-            cards = mw.col.addNote(newNote)
-            if not cards:
-                showWarning(_('''\
-                    The input you have provided would make an empty \
-                    question on all cards.'''), help='AddItems')
-                return
-
-            clearAudioQueue()
-            mw.col.autosave()
-            tooltip(_('Added'))
-
-        if self.currentQuickKey['editSource']:
-            EditCurrent(mw)
 
 
 def answerButtonList(self, _old):
