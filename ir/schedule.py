c8b2e92b0ffd5d4776c5936190e73f7a619da336
luoliyan <joseph.lorimer@posteo.net>
2018-12-16 15:30:44 +0930

    Fix division error; tweak formatting

--- ir/schedule.py
+++ ir/schedule.py
@@ -1,21 +1,24 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import unicode_literals
+from __future__ import division, unicode_literals
+
 from random import gauss, shuffle
 
 from PyQt4.QtCore import Qt
-from PyQt4.QtGui import (QAbstractItemView,
-                         QDialog,
-                         QDialogButtonBox,
-                         QHBoxLayout,
-                         QListWidget,
-                         QListWidgetItem,
-                         QPushButton,
-                         QVBoxLayout)
-
+from PyQt4.QtGui import (
+    QAbstractItemView,
+    QDialog,
+    QDialogButtonBox,
+    QHBoxLayout,
+    QListWidget,
+    QListWidgetItem,
+    QPushButton,
+    QVBoxLayout,
+)
 from anki.utils import stripHTML
 from aqt import mw
 from aqt.utils import showInfo, tooltip
+
 
 SCHEDULE_SOON = 1
 SCHEDULE_LATER = 2
@@ -44,13 +47,15 @@ class Scheduler:
         self.cardListWidget = QListWidget()
         self.cardListWidget.setAlternatingRowColors(True)
         self.cardListWidget.setSelectionMode(
-            QAbstractItemView.ExtendedSelection)
+            QAbstractItemView.ExtendedSelection
+        )
         self.cardListWidget.setWordWrap(True)
 
         posWidth = len(str(len(cardInfo) + 1))
         for i, card in enumerate(cardInfo, start=1):
             text = '❰ {} ❱\t{}'.format(
-                str(i).zfill(posWidth), stripHTML(card['title']))
+                str(i).zfill(posWidth), stripHTML(card['title'])
+            )
             item = QListWidgetItem(text)
             item.setData(Qt.UserRole, card)
             self.cardListWidget.addItem(item)
@@ -68,8 +73,9 @@ class Scheduler:
         controlsLayout.addWidget(downButton)
         controlsLayout.addWidget(randomizeButton)
 
-        buttonBox = QDialogButtonBox(QDialogButtonBox.Close |
-                                     QDialogButtonBox.Save)
+        buttonBox = QDialogButtonBox(
+            QDialogButtonBox.Close | QDialogButtonBox.Save
+        )
         buttonBox.accepted.connect(dialog.accept)
         buttonBox.rejected.connect(dialog.reject)
         buttonBox.setOrientation(Qt.Horizontal)
@@ -92,31 +98,39 @@ class Scheduler:
             self.reorder(cids)
 
     def _moveUp(self):
-        selected = [self.cardListWidget.item(i)
-                    for i in range(self.cardListWidget.count())
-                    if self.cardListWidget.item(i).isSelected()]
+        selected = [
+            self.cardListWidget.item(i)
+            for i in range(self.cardListWidget.count())
+            if self.cardListWidget.item(i).isSelected()
+        ]
         for item in selected:
             row = self.cardListWidget.row(item)
             newRow = max(0, row - 1)
             self.cardListWidget.insertItem(
-                newRow, self.cardListWidget.takeItem(row))
+                newRow, self.cardListWidget.takeItem(row)
+            )
             item.setSelected(True)
 
     def _moveDown(self):
-        selected = [self.cardListWidget.item(i)
-                    for i in range(self.cardListWidget.count())
-                    if self.cardListWidget.item(i).isSelected()]
+        selected = [
+            self.cardListWidget.item(i)
+            for i in range(self.cardListWidget.count())
+            if self.cardListWidget.item(i).isSelected()
+        ]
         selected.reverse()
         for item in selected:
             row = self.cardListWidget.row(item)
             newRow = min(self.cardListWidget.count(), row + 1)
             self.cardListWidget.insertItem(
-                newRow, self.cardListWidget.takeItem(row))
+                newRow, self.cardListWidget.takeItem(row)
+            )
             item.setSelected(True)
 
     def _randomize(self):
-        allItems = [self.cardListWidget.takeItem(0)
-                    for i in range(self.cardListWidget.count())]
+        allItems = [
+            self.cardListWidget.takeItem(0)
+            for i in range(self.cardListWidget.count())
+        ]
         shuffle(allItems)
         for item in allItems:
             self.cardListWidget.addItem(item)
@@ -152,7 +166,7 @@ class Scheduler:
         cids = [c['id'] for c in self._getCardInfo(card.did)]
         mw.col.sched.forgetCards(cids)
         cids.remove(card.id)
-        newOrder = cids[:newPos-1] + [card.id] + cids[newPos-1:]
+        newOrder = cids[: newPos - 1] + [card.id] + cids[newPos - 1 :]
         mw.col.sched.sortCards(newOrder)
 
     def reorder(self, cids):
@@ -163,12 +177,16 @@ class Scheduler:
         cardInfo = []
 
         for cid, nid in mw.col.db.execute(
-                'select id, nid from cards where did = ?',
-                did):
+            'select id, nid from cards where did = ?', did
+        ):
             note = mw.col.getNote(nid)
             if note.model()['name'] == self.settings['modelName']:
-                cardInfo.append({'id': cid,
-                                 'nid': nid,
-                                 'title': note[self.settings['titleField']]})
+                cardInfo.append(
+                    {
+                        'id': cid,
+                        'nid': nid,
+                        'title': note[self.settings['titleField']],
+                    }
+                )
 
         return cardInfo
