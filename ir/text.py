762cfc3f997a2f8faefb4aabf58cdd91b39ae666
Abdo <abd.nh25@gmail.com>
2022-06-26 02:13:26 +0300

    Prevent quick keys from triggering outside reviewer

--- ir/text.py
+++ ir/text.py
@@ -53,6 +53,9 @@ class TextManager:
         self.save()
 
     def extract(self, settings=None):
+        if mw.state != 'review':
+            return
+
         if not settings:
             settings = self.settings
 
